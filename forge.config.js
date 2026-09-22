39eaf9b2a08f3b25f02491f120750ef84feda688
Shahriyar-Rahim <rahimislam420.ri35@gmail.com>
2026-09-22 20:21:39 +0600

    ci: fix action reference syntax in release workflow

--- forge.config.js
+++ forge.config.js
@@ -23,5 +23,32 @@ module.exports = {
       name: "@electron-forge/maker-zip",
       platforms: ["linux"],
     },
+
+    // --- Windows Makers (.exe Installer & Portable Zip) ---
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "ScientificCalculator",
+        setupIcon: "./assets/icon.ico",
+      },
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["win32"],
+    },
+
+    // --- macOS Makers (.dmg Installer & Zip) ---
+    {
+      name: "@electron-forge/maker-dmg",
+      config: {
+        name: "ScientificCalculator",
+        icon: "./assets/icon.icns",
+        format: "ULFO",
+      },
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
   ],
 };
